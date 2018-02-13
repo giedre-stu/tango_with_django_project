77e2f5fb929d56e431f31eb974eea5e3833ea8ff
Giedre Stulgyte <2285161s@student.gla.ac.uk>
2018-02-13 10:31:41 +0000

    fixed 5

--- populate_rango.py
+++ populate_rango.py
@@ -15,67 +15,63 @@ def populate():
 	# through each data structure, and add the data to our models.
 
 	python_pages = [
-		{"title": "Official Python Tutorial",
-		"url": "http://docs.python.org/2/tutorial/"},
-		{"title": "How to Think Like a Computer Scientist",
-		"url": "http://www.greenteapress.com/thinkpython/"},
-		{"title": "Learn Python in 10 Minutes",
-		"url": "http://www.korokithakis.net/tutorials/python/"}]
+        {"title": "Official Python Tutorial",
+        "url": "http://docs.python.org/2/tutorial/", "views":20},
+        {"title": "How to Think Like a Computer Scientist",
+        "url": "http://www.greenteapress.com/thinkpython/", "views":40},
+        {"title": "Learn Python in 10 Minutes",
+        "url": "http://www.korokithakis.net/tutorials/python/", "views":62}]
 
-	django_pages = [
-		{"title": "Official Django Tutorial",
-		"url": "http://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
-		{"title": "Django Rocks",
-		"url": "http://www.djangorocks.com/"},
-		{"title": "How to Tango with Django",
-		"url": "http://www.tangowithdjango.com/"}]
+    django_pages = [
+        {"title": "Official Django Tutorial",
+        "url": "http://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views":23},
+        {"title": "Django Rocks",
+        "url": "http://www.djangorocks.com/", "views":63},
+        {"title": "How to Tango with Django",
+        "url": "http://www.tangowithdjango.com/", "views":11}]
 
-	other_pages = [
-		{"title": "Bottle",
-		"url": "http://bottlepy.org/docs/dev/"},
-		{"title": "Flask",
-		"url": "http://flask.pocoo.org"}]
+    other_pages = [
+        {"title": "Bottle",
+        "url": "http://bottlepy.org/docs/dev/", "views":8},
+        {"title": "Flask",
+        "url": "http://flask.pocoo.org", "views":33}]
 
-	cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
-			"Django": {"pages": django_pages, "views":64,"likes":32},
-			"Other Frameworks": {"pages": other_pages, "views":32,"likes":16}}
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views":64,"likes":32},
+            "Other Frameworks": {"pages": other_pages, "views":32,"likes":16}}
 
-		# If you want to add more catergories or pages,
-		# add them to the dictionaries above.
-		# The code below goes through the cats dictionary, then adds each category,
-		# and then adds all the associated pages for that category.
-		# if you are using Python 2.x then use cats.iteritems() see
-		# http://docs.quantifiedcode.com/python-anti-patterns/readability/	
-		# for more information about how to iterate over a dictionary properly.
+# If you want to add more categories or pages, add them to the dictionaries above.
 
-	for cat, cat_data in cats.items():
-		c = add_cat(cat, cat_data["views"], cat_data["likes"]) 
-		for p in cat_data["pages"]:
-			add_page(c, p ["title"], p["url"])
+# The code below goes through the cats dictionary then adds each category.
+# http://docs.quantifiedcode.com/python-anti-patterns/readability/ for more info.
 
-		# Print out the categories we have added.
-	for c in Category.objects.all():
-		for p in Page.objects.filter(category=c):
-			print ("- {0} - {1}".format(str(c), str(p)))
+    for cat, cat_data in cats.items():
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
+        for p in cat_data["pages"]:
+            add_page(c, p ["title"], p["url"],p["views"])
+
+    # Print out the categories we have added
+
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print ("- {0} - {1}".format(str(c), str(p)))
 
 def add_page(cat, title, url, views=0):
-	p = Page.objects.get_or_create(category=cat, title=title) [0]
-	p.url=url
-	p.views=views
-	p.save()
-	return p
+    p = Page.objects.get_or_create(category=cat, title=title) [0]
+    p.url=url
+    p.views=views
+    p.save()
+    return p
 
-def add_cat(name, views=0, likes=0): # Modified by JNR 22.01.2018
-	c = Category.objects.get_or_create(name=name)[0]
-	c.views=views # Added by JNR 22.01.2018
-	c.likes=likes # Added by JNR 22.01.2018
-	c.save()
-	return c
+def add_cat(name, views=0, likes=0): 
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
+    c.save()
+    return c
 
-# Start execution here!
+# Start execution here
+
 if __name__ == '__main__':
-	print("Starting Rango population script...")
-	populate()
-  
-
-
+    print("Starting Rango population script...")
+    populate()
--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rango.models import Category
+from rango.models import Page
+
 def index(request):
 	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
 	return render(request, 'rango/index.html', context=context_dict)
+
+	category_list = Category.objects.order_by('-likes') [:5]
+	page_list = Page.objects.order_by('-views') [:5]
+	context_dict = {'categories': category_list, 'pages':page_list}
+
+	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
 	return render(request, 'rango/about.html') #about page
