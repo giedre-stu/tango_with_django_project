9d02b74f1e53734578dbddc40a75e49d072c338c
Giedre Stulgyte <2285161s@student.gla.ac.uk>
2018-02-13 12:03:39 +0000

    chapter 6

--- rango/views.py
+++ rango/views.py
@@ -16,3 +16,18 @@ def index(request):
 
 def about(request):
 	return render(request, 'rango/about.html') #about page
+
+def show_category(request, category_name_slug):
+	context_dict = {}
+
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+
+	return render(request, 'rango/category.html', context_dict)
