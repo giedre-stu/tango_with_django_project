1e93376ffe6961a1129966c130c4ed1d9f6af546
Giedre Stulgyte <2285161s@student.gla.ac.uk>
2018-02-13 00:47:11 +0000

    chapter 5

--- rango/models.py
+++ rango/models.py
@@ -6,8 +6,14 @@ from django.db import models
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
 
-def __unicode__(self): 
-	return self.name
+	class Meta:
+		verbose_name_plural = 'Categories'
+		
+	def __str__(self):
+		return self.name
+
+def __unicode__(self):
+		return self.name
 
 class Page(models.Model):
 	category = models.ForeignKey(Category) 
